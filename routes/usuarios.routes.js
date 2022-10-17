e6af6fcfedb0e368d79e05a02892c9c561ce2354
Jesús Andrés Mata <andresmata.c21@gmail.com>
2022-10-17 15:19:50 -0500

    Controladores en las rutas de usuario

--- routes/usuarios.routes.js
+++ routes/usuarios.routes.js
@@ -1,7 +1,10 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
 
-const { obtenerUsuarios } = require('../controllers/usuarios');
+const { obtenerUsuarios,
+  actualizarUsuarios,
+  crearUsuarios,
+  eliminarUsuarios } = require('../controllers/usuarios');
 
 // const { tieneRole, validarCampos, validarJWT } = require('../middlewares');
 
@@ -11,34 +14,29 @@ const router = Router();
 
 router.get('/', obtenerUsuarios);
 
-// Si se envían dos argumentos, uno es la ruta y el otro es el controlador
-// router.put('/:id', [
-//   check('id', 'No es un ID válido').isMongoId(),
-//   check('id').custom(existeUsuarioPorId),
-//   check('rol').custom(esRolValido),
-//   validarCampos
-// ], usuariosPut);
+router.put('/:id', [
+  check('id', 'No es un ID válido').isMongoId(),
+  // check('id').custom(existeUsuarioPorId),
+  // check('rol').custom(esRolValido),
+  // validarCampos
+], actualizarUsuarios);
 
-// Si se envían 3: 1 ruta, 2 middlewares y 3 controlador
-// router.post('/', [
-//   // Verificamos el campo correo del body
-//   check('nombre', 'El nombre es obligatorio.').not().isEmpty(),
-//   check('password', 'El password debe de ser más de 6 letras.').isLength({ min: 6 }),
-//   check('correo', 'El correo no es válido.').isEmail(),
-//   check('correo').custom(emailExiste),
-//   // check('rol', 'No es un rol permitido.').isIn(['ADMIN_ROLE', 'USER_ROLE']),
-//   check('rol').custom(esRolValido),
-//   validarCampos
-// ], usuariosPost);
+router.post('/', [
+  check('nombre', 'El nombre es obligatorio.').not().isEmpty(),
+  check('password', 'El password debe de ser más de 6 letras.').isLength({ min: 6 }),
+  check('correo', 'El correo no es válido.').isEmail(),
+  // check('correo').custom(emailExiste),
+  // check('rol').custom(esRolValido),
+  // validarCampos
+], crearUsuarios);
 
-// router.delete('/:id', [
-//   validarJWT,
-//   // esAdminRole,
-//   tieneRole('ADMIN_ROLE', 'VENTAS_ROLE'),
-//   check('id', 'No es un ID válido').isMongoId(),
-//   check('id').custom(existeUsuarioPorId),
-//   validarCampos
-// ], usuariosDelete);
+router.delete('/:id', [
+  // validarJWT,
+  // tieneRole('ADMIN_ROLE', 'VENTAS_ROLE'),
+  check('id', 'No es un ID válido').isMongoId(),
+  // check('id').custom(existeUsuarioPorId),
+  // validarCampos
+], eliminarUsuarios);
 
 
 module.exports = router;
