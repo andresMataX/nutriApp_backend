4a6577df43f240f2452311bc4b1d84d9d14ecf1c
Jesús Andrés Mata <andresmata.c21@gmail.com>
2022-10-17 14:46:39 -0500

    Levantamiento mínimo del servidor

---
+++ models/server.js
@@ -0,0 +1,55 @@
+const express = require('express')
+var cors = require('cors');
+const { dbConnection } = require('../database/config.db');
+
+class Server {
+
+    constructor() {
+        this.app = express();
+        this.port = process.env.PORT;
+
+        this.paths = {
+            auth: '/api/auth',
+            buscar: '/api/buscar',
+            categorias: '/api/categorias',
+            usuarios: '/api/usuarios',
+            productos: '/api/productos',
+        }
+
+        this.conectarDB();
+
+        this.middlewares();
+
+        this.routes();
+    }
+
+    async conectarDB() {
+
+        await dbConnection();
+    }
+
+    middlewares() {
+
+        this.app.use(cors())
+
+        this.app.use(express.json());
+
+        this.app.use(express.static('public'))
+    }
+
+    routes() {
+
+        // this.app.use(this.paths.buscar, require('../routes/buscar.routes'));
+
+    }
+
+    listen() {
+
+        this.app.listen(this.port, () => {
+            console.log(`Servidor corriendo en: ${this.port}`);
+        })
+
+    }
+}
+
+module.exports = Server;
