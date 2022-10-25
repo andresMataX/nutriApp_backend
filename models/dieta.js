4a527a367e7f69957e6d814cd4eed77811db47a5
Jesús Andrés Mata <andresmata.c21@gmail.com>
2022-10-24 19:59:09 -0500

    Relación inicial con modelo de Desayuno

---
+++ models/dieta.js
@@ -0,0 +1,24 @@
+const { Schema, model } = require('mongoose');
+
+const DietaSchema = Schema({
+  usuario: {
+    type: Schema.Types.ObjectId,
+    ref: 'Usuario',
+    required: true
+  },
+  desayunos: [{
+    type: Schema.Types.ObjectId,
+    ref: 'Desayuno'
+  }]
+});
+
+DietaSchema.methods.toJSON = function () {
+  const { __v, _id, ...dieta } = this.toObject();
+  dieta.uid = _id;
+  return {
+    dieta,
+  };
+}
+
+
+module.exports = model('Dieta', DietaSchema);
