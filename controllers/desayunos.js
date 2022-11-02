11a03d09c20d67d682fa25009c9b6308992a2f65
Jesús Andrés Mata <andresmata.c21@gmail.com>
2022-11-02 00:00:24 -0600

    Obtención de desayunos por semana

--- controllers/desayunos.js
+++ controllers/desayunos.js
@@ -1,11 +1,30 @@
 const { response } = require("express");
 const Desayuno = require("../models/desayuno");
+const Dieta = require("../models/dieta");
 
 const obtenerDesayunos = async (req, res = response) => {
 
   const desayunos = await Desayuno.find();
   res.json(desayunos);
 
+}
+
+const obtenerDesayunoPorSemana = async (req, res = response) => {
+
+  const semana = req.params.semana;
+
+  // Obtención del desayuno que coincide con la semana
+  const desayunoSemana = await Dieta.findOne({ semana });
+
+  if (!desayunoSemana) {
+    return res.status(400).json({
+      msg: `La semana ${semana}, no existe.`
+    });
+  }
+
+  const desayuno = await Desayuno.findById(desayunoSemana.desayuno);
+
+  res.json(desayuno);
 }
 
 const crearDesayuno = async (req, res = response) => {
@@ -22,5 +41,6 @@ const crearDesayuno = async (req, res = response) => {
 
 module.exports = {
   obtenerDesayunos,
-  crearDesayuno
+  crearDesayuno,
+  obtenerDesayunoPorSemana
 }
--- routes/desayunos.routes.js
+++ routes/desayunos.routes.js
@@ -1,7 +1,7 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
 
-const { obtenerDesayunos, crearDesayuno } = require('../controllers/desayunos');
+const { obtenerDesayunos, crearDesayuno, obtenerDesayunoPorSemana } = require('../controllers/desayunos');
 const { validarJWT, tieneRole } = require('../middlewares');
 const { validarCampos } = require('../middlewares/validar-campos');
 
@@ -12,6 +12,13 @@ router.get('/', [
   // tieneRole('ADMIN_ROLE'),
   validarCampos
 ], obtenerDesayunos);
+
+router.get('/:semana', [
+  // validarJWT,
+  // tieneRole('ADMIN_ROLE'),
+  check('semana', 'La semana es obligatoria').not().isEmpty(),
+  validarCampos
+], obtenerDesayunoPorSemana);
 
 router.post('/', [
   // validarJWT,
